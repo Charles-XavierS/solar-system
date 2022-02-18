bfb3eaae174405ad8ec5019252f6d49235de4527
Charles Xavier <charlesxsaldanha@hotmail.com>
2022-02-18 16:31:21 -0300

    Requisito 7 - Criando componente Missions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from './components/Header';
+import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
@@ -10,6 +11,8 @@ class App extends React.Component {
         <Header />
 
         <SolarSystem />
+
+        <Missions />
       </div>
     );
   }
