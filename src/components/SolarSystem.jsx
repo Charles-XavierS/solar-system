475575fc1ca3f5e77ce36546305c1e5cf6925e5b
Charles Xavier <charlesxsaldanha@hotmail.com>
2022-02-18 15:38:00 -0300

    Requisitos 2 e 3 - Componentes SolarSystem e Title

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,1 +1,15 @@
-import planets from '../data/planets';
+import React from 'react';
+// import Title
+// import planets from '../data/planets';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <h1>asdasdasd</h1>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
